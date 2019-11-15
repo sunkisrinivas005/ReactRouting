aa7f8dbad2cb78a9e449a1c5dcff341ca3b10a5a
srinivas sunki <sunkisrinivas005@gmail.com>
2019-11-15 22:28:06 +0530

    changes in data & added sorting for parcels

--- src/landingPage.js
+++ src/landingPage.js
@@ -8,12 +8,13 @@ class LandingPage extends React.Component {
   state = {
     filteredInfo: null,
     sortedInfo: null,
-    filterData : []
+    filterData : [],
+    AgenciesData : []
   };
 
   componentWillMount = () => {
     let filterData = data.map(i => i.ULB).map(i => {
-      return {text : i.toUpperCase() , value : i}
+      return {text :i , value : i}
     });
      this.setState({
       filterData
@@ -22,7 +23,6 @@ class LandingPage extends React.Component {
   }
 
   handleChange = (pagination, filters, sorter) => {
-    console.log('Various parameters', pagination, filters, sorter);
     this.setState({
       filteredInfo: filters,
       sortedInfo: sorter,
@@ -59,30 +59,34 @@ class LandingPage extends React.Component {
         dataIndex: 'ULB',
         key: 'ULB',
         filters: filterData,
-        filteredValue: filteredInfo.name || null,
-        onFilter: (value, record) => record.name.includes(value),
-        sorter: (a, b) => a.name.length - b.name.length,
+        filteredValue: filteredInfo.ULB || null,
+         onFilter: (value, record) => record.ULB.includes(value),
         sortOrder: sortedInfo.columnKey === 'ULB' && sortedInfo.order,
+        sorter: (a, b) => a.ULB.length - b.ULB.length,
+        sortDirections: ['descend'],
         ellipsis: true,
       },
       {
         title: 'State',
         dataIndex: 'State',
-        key: 'age',
-        sorter: (a, b) => a.age - b.age,
-        sortOrder: sortedInfo.columnKey === 'age' && sortedInfo.order,
+        key: 'State',
         ellipsis: true,
       },
       {
         title: 'Parcels',
         dataIndex: 'Parcels',
         key: 'Parcels',
+        sorter: (a, b) => a.Parcels - b.Parcels,
+        sortOrder: sortedInfo.columnKey === 'Parcels' && sortedInfo.order,
         ellipsis: true,
       },
       {
         title: 'Agencies',
         dataIndex: 'Agencies',
         key: 'Agencies',
+        filters: [{text: "Agency-1", value : "Agency-1"}, {text: "Agency-2", value :"Agency-2"}],
+        filteredValue: filteredInfo.Agencies || null,
+        onFilter: (value, record) => record.Agencies.includes(value),
         ellipsis: true,
       },
       {
@@ -95,17 +99,23 @@ class LandingPage extends React.Component {
         title: 'Agents',
         dataIndex: 'Agents',
         key: 'Agents',
+        sorter: (a, b) => a.Agents - b.Agents,
+        sortOrder: sortedInfo.columnKey === 'Agents' && sortedInfo.order,
+        ellipsis: true,
+      },{
+        title: 'Lifetime',
+        dataIndex: 'Lifetime',
+        key: 'Lifetime',
+        filters: [{text: "Level-1", value : "Level-1"}, {text: "Level-2", value :"Level-2"}],
+        filteredValue: filteredInfo.Lifetime || null,
+        onFilter: (value, record) => record.Lifetime.includes(value),
         ellipsis: true,
       }
     ];
     return (
-      <div style = {{marginTop : "100px", width: "95%", marginLeft: "3%"}}>
-      <div className="table-operations">
-          <Button onClick={this.setAgeSort}>Sort age</Button>
-          <Button onClick={this.clearFilters}>Clear filters</Button>
-          <Button onClick={this.clearAll}>Clear filters and sorters</Button>
-        </div>
-        <Table columns={columns} dataSource={data} onChange={this.handleChange} />
+      <div style = {{marginTop: "100px", width: "95%", marginLeft: "3%"}}>
+      <h3 style ={{display: "flex", alignSelf: "center", flexDirection : "row", justifyContent : "center", fontFamily : "Lato"}}>ULB Result Sheet</h3>
+        <Table columns={columns} dataSource={data} onChange={this.handleChange} style ={{marginTop :"25px"}} />
       </div>
     );
   }
